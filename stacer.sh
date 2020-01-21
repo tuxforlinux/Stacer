b25533d163d7f9405f8f2191926ff220829f68b3
Archiman Chakraborty <60009784+tuxforlinux@users.noreply.github.com>
2020-01-21 14:40:12 +0530

    Create stacer.sh

---
+++ stacer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This Script Will Install Scater - Linux System Optimizer & Monitoring"
+
+
+sudo add-apt-repository ppa:oguzhaninan/stacer
+
+sudo apt-get update -y
+
+sudo apt-get install stacer -y
+
+sudo apt-get update -y
+
+
+echo "Thank You For Installing Stacer - Linux System Optimizer & Monitoring"
